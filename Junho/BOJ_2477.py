1c7d75c77180a8630b0c483dec429c00fde53ed3
nietsopjou <103643092+nietsopjou@users.noreply.github.com>
2022-08-29 08:31:15 +0900

    W9 Solution_BOJ_2477

---
+++ Junho/BOJ_2477.py
@@ -0,0 +1,16 @@
+n = int(input())
+mapping = []
+for i in range(6):
+    mapping.append(list(map(int, input().split())))
+
+#find index of max in east-west and north-south repectively
+ew_map_length = [[x,[w,v]] for x,[w,v] in enumerate(mapping) if w <= 2]
+temp_max = max([v for [x, [w,v]] in ew_map_length])
+ew_index = [[x,[w,v]] for x,[w,v] in ew_map_length if v == temp_max][0][0]
+
+sn_map_length = [[x, [w,v]] for x,[w,v] in enumerate(mapping) if w >  2]
+temp_max = max([v for [x, [w,v]] in sn_map_length])
+sn_index = [[x,[w,v]] for x,[w,v] in sn_map_length if v == temp_max][0][0]
+
+result = mapping[ew_index][1] * mapping[sn_index][1] - mapping[(ew_index+3)%6][1] * mapping[(sn_index+3)%6][1]
+print(result*n)
