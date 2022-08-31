b6f8a5ac2c1a782a56eb20fe5f2a587c6d7f641a
Yumin Lee <55529646+yourmean@users.noreply.github.com>
2022-08-31 23:05:25 +0900

    220831

--- Yumin/BOJ_09461.py
+++ Yumin/BOJ_09461.py
@@ -2,7 +2,7 @@ ans = [0 for i in range(101)]
 ans[1], ans[2], ans[3]  = 1, 1, 1
 
 for i in range(0, 98):
-    ans[i + 3] = ans[i] + ans[i + 1]
+    ans[i+3] = ans[i] + ans[i+1]
 
 for i in range(int(input())):
     n = int(input())
