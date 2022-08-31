206e035db02aef21093c983925b9f53351e25f4a
yourmean <lym0112@uos.ac.kr>
2022-08-31 23:04:05 +0900

    220831

---
+++ Yumin/BOJ_02477.py
@@ -0,0 +1,25 @@
+n = int(input())
+board = [list(map(int,input().split())) for _ in range(6)]
+
+max_width, max_width_idx = 0, 0
+max_height, max_height_idx = 0, 0
+
+for i in range(len(board)):
+    if board[i][0] in [1,2]:
+        if board[i][1] > max_width:
+            max_width = board[i][1]
+            max_width_idx = i
+    else:
+        if board[i][1] > max_height:
+            max_height = board[i][1]
+            max_height_idx = i
+
+min_width = abs(board[(max_width_idx-1) % 6][1] - board[(max_width_idx+1) % 6][1])
+min_height = abs(board[(max_height_idx-1) % 6][1] - board[(max_height_idx+1) % 6][1])
+
+
+print((max_height*max_width) - (min_height * min_width) * n)
+
+
+
+# (1)%6 = 5
---
+++ Yumin/BOJ_11399.py
@@ -0,0 +1,8 @@
+n = int(input())
+line = list(map(int, input().split()))
+line.sort()
+
+ans = 0
+for i in range(1, n+1):
+    ans += sum(line[:i])
+print(ans)
