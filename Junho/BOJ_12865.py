9a80c18cad7afff6c774db92f5d0d70b7da35952
nietsopjou <103643092+nietsopjou@users.noreply.github.com>
2022-09-14 19:36:20 +0900

    평범한 배낭

---
+++ Junho/BOJ_12865.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+backpack = []
+dp = [[0]*(k+1) for _ in range(n+1)]
+
+for i in range(n):
+    backpack.append(list(map(int, input().split())))
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        w = backpack[i-1][0]
+        v = backpack[i-1][1]
+
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
+
+print(dp[n][k])
